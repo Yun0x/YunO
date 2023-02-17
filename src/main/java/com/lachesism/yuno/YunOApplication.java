954892773c6b395e2bfb7f6e5ce40faf10b1801e
YunOx <9668049+yunox@user.noreply.gitee.com>
2023-02-17 14:49:14 +0800

    first commit

---
+++ src/main/java/com/lachesism/yuno/YunOApplication.java
@@ -0,0 +1,13 @@
+package com.lachesism.yuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunOApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunOApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lachesism/yuno/YunOApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lachesism.yuno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunOApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
